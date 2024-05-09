b6d09a286b0795e0b559189c2ed5ec63bf8a6dcb
Himanshu Jaiswal <55348957+itshimanshu010@users.noreply.github.com>
2024-05-09 17:46:00 +0530

    Update 3.list.cpp

--- STL/3.list.cpp
+++ STL/3.list.cpp
@@ -1,8 +1,26 @@
 //List is similar to vector
+//List gives front operations too
+//This container is also dynamic in nature
 #include<bits/stdc++.h> 
 using namespace std;
 
 int main(){
-  
+
+    //decalartion
+    list<int> ls;
+
+    ls.push_back(2); // {2}
+    ls.emplace_back(4); //{2,4}
+
+    ls.push_front(5); //{5,2,4} // very cheap in term of time complexity
+    //directly push front to front but in vector we have to use insert function for this which is much costiler
+    //in list, since the internal operation of list is doubly linked list
+    //doubly linked list is maintained for a list
+    //but for a vector singly linked list is maintained
+
+    ls.emplace_front();  //{2,4}
+
+    //rest functions are
+    //begin,end,rebegin,rend,clear,insert,size,swap
 
 }
