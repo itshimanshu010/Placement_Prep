582feb98a4a1bd01d9c97cced14d5d6e580a2039
Himanshu Jaiswal <55348957+itshimanshu010@users.noreply.github.com>
2024-08-05 16:15:43 +0530

    Create reverseUsingTwoVariable.cpp

---
+++ C++ & DSA/4.Basic Recursion/reverseUsingTwoVariable.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void rev(int l,int r, int arr[], int n){
+    if(l>=r){
+        return;
+    }
+    swap(arr[l],arr[r]);
+    rev(l+1,r-1,arr,n);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    
+    for (int i =0; i<n;i++){
+        cin>> arr[i];
+    }
+    rev(0,n-1,arr,n);
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
